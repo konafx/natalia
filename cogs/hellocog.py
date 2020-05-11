ccc9645e457cf11467f4f86fd34f8f040ed4981e
konafx <afxlieonon@gmail.com>
2020-05-12 03:51:21 +0900

    Ref https://qiita.com/Lazialize/items/81f1430d9cd57fbd82fb

---
+++ cogs/hellocog.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class HelloCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send('Bon tarde!')
+
+def setup(bot):
+    bot.add_cog(HelloCog(bot))
---
+++ natalia.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+
+import traceback
+import os
+import subprocess
+
+INITIAL_EXTENSIONS = [
+    'cogs.hellocog'
+]
+
+_DISCORD_TOKEN = os.environ['_DISCORD_TOKEN']
+
+class Natalia(commands.Bot):
+    def __init__(self, command_prefix):
+        super().__init__(command_prefix)
+
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+
+    async def on_ready(self):
+        print('-----')
+        print(self.user.name)
+        print(self.user.id)
+        print('-----')
+
+if __name__ == '__main__':
+    bot = Natalia(command_prefix='!')
+    bot.run(_DISCORD_TOKEN)
+
