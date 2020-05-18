fe3a62055b5125d1e6817942d09c4bcdb7a366e2
konafx <afxlieonon@gmail.com>
2020-05-18 18:25:49 +0900

    WIP: superchat chatcolor test, implement

--- cogs/superchatcog.py
+++ cogs/superchatcog.py
@@ -2,7 +2,18 @@ from discord import Embed
 from discord.ext import commands
 
 
-class SuperChatCog(commands.Cog):
+CHATCOLORS = {
+    1: 'BLUE',
+    200: 'AQUA',
+    500: 'GREEN',
+    1000: 'YELLOW',
+    2000: 'ORANGE',
+    5000: 'MAGENTA',
+    10000: 'RED'
+}
+
+
+class SuperchatCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -13,15 +24,24 @@ class SuperChatCog(commands.Cog):
                  '例: !superchat 50000\n'
             )
     async def superchat(self, ctx, tip: int, comment: str):
-        msg = self.create_embed()
+        color = chatcolor(tip)
         await ctx.send(embed=msg)
 
     @superchat.error
-    async def team_error(self, ctx, error):
+    async def superchat_error(self, ctx, error):
         print(f'{error=}')
         if isinstance(error, commands.BadArgument):
             await ctx.send('ナニ言ってるかワカラナイヨ…')
 
 
+def chatcolor(tip: int, chatcolors: dict = CHATCOLORS):
+    if (tip < 1 or tip > 50000):
+        raise ValueError('Range Over [1, 50000]')
+
+    allow_chatcolors = list(filter(lambda cc: cc[0] <= tip, chatcolors.items()))
+    color = allow_chatcolors.pop()[1]
+    return color
+
+
 def setup(bot):
-    bot.add_cog(SuperChatCog(bot))
+    bot.add_cog(SuperchatCog(bot))
